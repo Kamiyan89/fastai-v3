f581a42bb4aa2e83324e1782fae076d559107aef
Kamiyan89 <ccqr2@hotmail.com>
2019-07-17 21:05:04 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/4ki0bscva69wj3o/Eyes.pkl?raw=1'
+export_file_name = 'Eyes.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['center', 'down', 'downleft', 'downright', 'left', 'right', 'up', 'upleft', 'upright']
 path = Path(__file__).parent
 
 app = Starlette()
